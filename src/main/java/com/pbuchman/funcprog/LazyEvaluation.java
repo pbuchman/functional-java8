8b96182f6161cce93ffee136c8c265e5a4c728cb
Piotr Buchman <kontakt@pbuchman.com>
2014-11-18 09:43:08 +0100

    Streams - lazy evaluation

--- src/main/java/com/pbuchman/funcprog/LazyEvaluation.java
+++ src/main/java/com/pbuchman/funcprog/LazyEvaluation.java
@@ -1,32 +1,41 @@
 package com.pbuchman.funcprog;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static java.text.MessageFormat.format;
 
 /**
  * @author Piotr Buchman <kontakt@pbuchman.com>
  */
 public class LazyEvaluation {
-    public static void main(String[] args) {
 
+    private static int length(final String name) {
+        System.out.println(format("Getting length for {0}", name));
+        return name.length();
+    }
+
+    private static String toUpperCase(final String name) {
+        System.out.println(format("Converting to UC: {0}", name));
+        return name.toUpperCase();
+    }
+
+    public static void main(String[] args) {
         final List<String> ampera =
                 Arrays.asList("Michal", "Adam", "Kasia", "Maciek", "Piotrek", "Michal", "Kuba", "Mirek", "Marek");
-        // v2.0
-        ampera.forEach(new Consumer<String>() {
-            public void accept(final String name) {
-                System.out.println(name);
-            }
-        });
-        // v2.1
-        ampera.forEach((final String name) -> System.out.println(name));
-        // v2.2
-        ampera.forEach((name) -> System.out.println(name));
-        // v2.3
-        ampera.forEach(name -> System.out.println(name));
-        // v2.4
-        ampera.forEach(System.out::println);
 
+        Stream<String> namesWith5Letters =
+                ampera.stream()
+                        .filter(name -> length(name) == 5)
+                        .map(name -> toUpperCase(name));
+
+        System.out.println("Stream created, filtered, mapped...");
+        System.out.println("Ready to call findFirst...");
+
+        System.out.println(namesWith5Letters.findFirst().get());
     }
 }
